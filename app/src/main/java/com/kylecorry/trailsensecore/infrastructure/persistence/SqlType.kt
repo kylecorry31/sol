be4cd147381bde91c943552eae910c7f85b4431b
Kyle Corry <kylecorry31@gmail.com>
2020-09-20 13:02:15 -0400

    Add tests for cache

---
+++ app/src/main/java/com/kylecorry/trailsensecore/infrastructure/persistence/SqlType.kt
@@ -0,0 +1,18 @@
+package com.kylecorry.trailsensecore.infrastructure.persistence
+
+enum class SqlType {
+    Short,
+    Int,
+    Long,
+    Float,
+    Double,
+    String,
+    Boolean,
+    NullableShort,
+    NullableInt,
+    NullableLong,
+    NullableFloat,
+    NullableDouble,
+    NullableString,
+    NullableBoolean,
+}
---
+++ app/src/main/java/com/kylecorry/trailsensecore/infrastructure/persistence/UserPreferencesBase.kt
@@ -0,0 +1,7 @@
+package com.kylecorry.trailsensecore.infrastructure.persistence
+
+import android.content.Context
+
+abstract class UserPreferencesBase(protected val context: Context) {
+    protected val cache = Cache(context)
+}
