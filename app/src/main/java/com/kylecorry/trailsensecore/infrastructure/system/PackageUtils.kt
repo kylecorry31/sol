9adfc5086a21e2cd92cb4af0d92aa138d76eef7f
Kyle Corry <kylecorry31@gmail.com>
2020-09-27 07:10:12 -0400

    Detect if a package is available

--- app/src/main/java/com/kylecorry/trailsensecore/infrastructure/system/PackageUtils.kt
+++ app/src/main/java/com/kylecorry/trailsensecore/infrastructure/system/PackageUtils.kt
@@ -1,6 +1,7 @@
 package com.kylecorry.trailsensecore.infrastructure.system
 
 import android.content.Context
+import android.content.pm.PackageManager
 
 object PackageUtils {
 
@@ -11,6 +12,15 @@ object PackageUtils {
     fun getVersionName(context: Context): String {
         val packageManager = context.packageManager
         return packageManager.getPackageInfo(getPackageName(context), 0).versionName
+    }
+
+    fun isPackageInstalled(context: Context, packageName: String): Boolean {
+        return try {
+            context.packageManager.getPackageInfo(packageName, 0)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
     }
 
 }
