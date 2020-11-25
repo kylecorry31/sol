19a041714c0a252471314e1c4c40c00744798c0b
Kyle Corry <kylecorry31@gmail.com>
2020-11-25 07:13:14 -0500

    Add sensor information to exception report

---
+++ app/src/main/java/com/kylecorry/trailsensecore/infrastructure/sensors/SensorDetailProvider.kt
@@ -0,0 +1,28 @@
+package com.kylecorry.trailsensecore.infrastructure.sensors
+
+import android.content.Context
+import android.hardware.Sensor
+import com.kylecorry.trailsensecore.infrastructure.flashlight.Flashlight
+import com.kylecorry.trailsensecore.infrastructure.sensors.SensorChecker
+import com.kylecorry.trailsensecore.infrastructure.system.PermissionUtils
+
+class SensorDetailProvider {
+
+    fun getSensorDetails(context: Context): String {
+        val sensorChecker = SensorChecker(context)
+        val locationPermission = PermissionUtils.isLocationEnabled(context)
+        val backgroundLocationPermission = PermissionUtils.isLocationEnabled(context)
+        val gps = sensorChecker.hasGPS()
+        val barometer = sensorChecker.hasBarometer()
+        val gravity =  sensorChecker.hasGravity()
+        val hygrometer = sensorChecker.hasHygrometer()
+        val magnetometer = sensorChecker.hasSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        val accelerometer = sensorChecker.hasSensor(Sensor.TYPE_ACCELEROMETER)
+        val orientation = sensorChecker.hasSensor(Sensor.TYPE_ORIENTATION)
+        val flashlight = Flashlight.hasFlashlight(context)
+
+        return "Location: $locationPermission\nBackground Location: $backgroundLocationPermission\nGPS: $gps\nBarometer: $barometer\nGravity: $gravity\nHygrometer: $hygrometer\nMagnetometer: $magnetometer\nAccelerometer: $accelerometer\nOrientation: $orientation\nFlashlight: $flashlight"
+    }
+
+
+}
--- app/src/main/java/com/kylecorry/trailsensecore/infrastructure/system/ExceptionUtils.kt
+++ app/src/main/java/com/kylecorry/trailsensecore/infrastructure/system/ExceptionUtils.kt
@@ -3,6 +3,7 @@ package com.kylecorry.trailsensecore.infrastructure.system
 import android.content.Context
 import android.os.Build
 import android.os.Looper
+import com.kylecorry.trailsensecore.infrastructure.sensors.SensorDetailProvider
 import java.time.Duration
 import kotlin.concurrent.thread
 import kotlin.system.exitProcess
@@ -32,9 +33,15 @@ object ExceptionUtils {
         val appVersion = PackageUtils.getVersionName(context)
         val message = throwable?.message ?: ""
         val stackTrace = throwable?.stackTraceToString() ?: ""
+        var sensors = ""
+        try {
+            sensors = SensorDetailProvider().getSensorDetails(context)
+        } catch (e: Exception){
+            // Don't do anything
+        }
 
         val body =
-            "Version: ${appVersion}\nDevice: ${device}\nAndroid SDK: ${androidVersion}\nMessage: ${message}\n\n$stackTrace"
+            "Version: ${appVersion}\nDevice: ${device}\nAndroid SDK: ${androidVersion}\nSensors\n$sensors\n\nMessage: ${message}\n\n$stackTrace"
 
         val intent = IntentUtils.email(
             email,
