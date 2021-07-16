497d39542dddff290617d46c3ad6710f0edbabf8
Kyle Corry <kylecorry31@gmail.com>
2021-07-16 07:18:24 -0400

    Add ids and operators to weights

--- app/src/main/java/com/kylecorry/trailsensecore/domain/units/WeightUnits.kt
+++ app/src/main/java/com/kylecorry/trailsensecore/domain/units/WeightUnits.kt
@@ -1,8 +1,8 @@
 package com.kylecorry.trailsensecore.domain.units
 
-enum class WeightUnits(val grams: Float) {
-    Pounds(453.592f),
-    Ounces(28.3495f),
-    Kilograms(1000f),
-    Grams(1f)
+enum class WeightUnits(val id: Int, val grams: Float) {
+    Pounds(1, 453.592f),
+    Ounces(2, 28.3495f),
+    Kilograms(3, 1000f),
+    Grams(4, 1f)
 }
