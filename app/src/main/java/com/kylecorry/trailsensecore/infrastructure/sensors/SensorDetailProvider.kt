f648776bd490c1641ce302e5b871449a5e81fb0e
Kyle Corry <kylecorry31@gmail.com>
2021-08-17 06:50:51 -0400

    Upgrade Andromeda

--- app/src/main/java/com/kylecorry/trailsensecore/infrastructure/sensors/SensorDetailProvider.kt
+++ app/src/main/java/com/kylecorry/trailsensecore/infrastructure/sensors/SensorDetailProvider.kt
@@ -4,22 +4,21 @@ import android.content.Context
 import android.hardware.Sensor
 import com.kylecorry.andromeda.location.GPS
 import com.kylecorry.andromeda.permissions.Permissions
-import com.kylecorry.andromeda.sense.SensorChecker
+import com.kylecorry.andromeda.sense.Sensors
 
 class SensorDetailProvider {
 
     @Suppress("DEPRECATION")
     fun getSensorDetails(context: Context): String {
-        val sensorChecker = SensorChecker(context)
         val locationPermission = Permissions.canGetFineLocation(context)
         val backgroundLocationPermission = Permissions.isBackgroundLocationEnabled(context)
         val gps = GPS.isAvailable(context)
-        val barometer = sensorChecker.hasBarometer()
-        val gravity =  sensorChecker.hasGravity()
-        val hygrometer = sensorChecker.hasHygrometer()
-        val magnetometer = sensorChecker.hasSensor(Sensor.TYPE_MAGNETIC_FIELD)
-        val accelerometer = sensorChecker.hasSensor(Sensor.TYPE_ACCELEROMETER)
-        val orientation = sensorChecker.hasSensor(Sensor.TYPE_ORIENTATION)
+        val barometer = Sensors.hasBarometer(context)
+        val gravity =  Sensors.hasGravity(context)
+        val hygrometer = Sensors.hasHygrometer(context)
+        val magnetometer = Sensors.hasSensor(context, Sensor.TYPE_MAGNETIC_FIELD)
+        val accelerometer = Sensors.hasSensor(context, Sensor.TYPE_ACCELEROMETER)
+        val orientation = Sensors.hasSensor(context, Sensor.TYPE_ORIENTATION)
 
         return "Location: $locationPermission\nBackground Location: $backgroundLocationPermission\nGPS: $gps\nBarometer: $barometer\nGravity: $gravity\nHygrometer: $hygrometer\nMagnetometer: $magnetometer\nAccelerometer: $accelerometer\nOrientation: $orientation"
     }
