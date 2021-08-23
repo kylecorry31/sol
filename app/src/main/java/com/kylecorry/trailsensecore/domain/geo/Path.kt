fa7464164c87f71bbb82253166c6d3e4910ab4fe
Kyle Corry <kylecorry31@gmail.com>
2021-08-23 15:18:14 -0400

    Revert "Add needed Path fields"
    This reverts commit 397f1f5c40f1e6982500dd912e17e97ed815b3cb.

--- app/src/main/java/com/kylecorry/trailsensecore/domain/geo/Path.kt
+++ app/src/main/java/com/kylecorry/trailsensecore/domain/geo/Path.kt
@@ -1,9 +1,6 @@
 package com.kylecorry.trailsensecore.domain.geo
 
 import androidx.annotation.ColorInt
-import com.kylecorry.andromeda.core.units.Distance
-import java.time.Duration
-import java.time.Instant
 
 data class Path(
     val id: Long,
@@ -11,37 +8,6 @@ data class Path(
     val points: List<PathPoint>,
     @ColorInt val color: Int,
     val style: PathStyle = PathStyle.Solid,
-    val deleteAfter: Duration? = null,
-    val owner: PathOwner = PathOwner.User,
-    val visible: Boolean = true
-) {
-    val distance: Distance
-        get() {
-            if (points.size < 2) {
-                return Distance.meters(0f)
-            }
-
-            var distance = 0f
-            for (i in 0 until points.lastIndex) {
-                distance += points[i].coordinate.distanceTo(points[i + 1].coordinate)
-            }
-
-            return Distance.meters(distance)
-        }
-
-    val lastPoint = points.lastOrNull()
-
-    val duration: Duration?
-        get() {
-            val start = points.firstOrNull()?.time ?: return null
-            val end = lastPoint?.time ?: return null
-
-            return Duration.between(start, end)
-        }
-
-    val deleteOn: Instant?
-        get() {
-            deleteAfter ?: return null
-            return lastPoint?.time?.plus(deleteAfter)
-        }
-}
+    val temporary: Boolean = false,
+    val owner: PathOwner = PathOwner.User
+)
