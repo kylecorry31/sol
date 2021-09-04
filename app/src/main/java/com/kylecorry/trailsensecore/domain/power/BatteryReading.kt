6397c95c43ae862ae9e5b34c23ca90c67f721fa9
Kyle Corry <kylecorry31@gmail.com>
2021-09-04 07:02:05 -0400

    Move power to TS

--- app/src/main/java/com/kylecorry/trailsensecore/domain/power/BatteryReading.kt
+++
@@ -1,5 +0,0 @@
-package com.kylecorry.trailsensecore.domain.power
-
-import java.time.Instant
-
-data class BatteryReading(val time: Instant, val percent: Float, val capacity: Float, val isCharging: Boolean)
