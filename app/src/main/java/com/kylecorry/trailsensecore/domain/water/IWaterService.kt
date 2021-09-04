fc64082ceddeddbaee3b9357f32a4197ae23d6d1
Kyle Corry <kylecorry31@gmail.com>
2021-09-04 07:04:31 -0400

    Remove water service

--- app/src/main/java/com/kylecorry/trailsensecore/domain/water/IWaterService.kt
+++
@@ -1,8 +0,0 @@
-package com.kylecorry.trailsensecore.domain.water
-
-import com.kylecorry.andromeda.core.units.Distance
-import java.time.Duration
-
-interface IWaterService {
-    fun getPurificationTime(altitude: Distance?): Duration
-}
