740649ea542617e25f15abf5fa9862029ab43fde
Kyle Corry <kylecorry31@gmail.com>
2021-09-25 10:36:04 -0400

    Improve humidity forecast

--- science/src/main/java/com/kylecorry/sol/science/meteorology/forecast/HumidityWeatherForecaster.kt
+++ science/src/main/java/com/kylecorry/sol/science/meteorology/forecast/HumidityWeatherForecaster.kt
@@ -1,59 +1,21 @@
 package com.kylecorry.sol.science.meteorology.forecast
 
-import com.kylecorry.sol.math.SolMath
 import com.kylecorry.sol.units.Reading
-import java.time.Duration
-import kotlin.math.absoluteValue
 
-class HumidityWeatherForecaster(
-    private val changeThreshold: Float = 3f,
-    private val stormThreshold: Float = 8f,
-) : IWeatherForecaster<Float> {
+class HumidityWeatherForecaster : IWeatherForecaster<Float> {
 
     override fun forecast(readings: List<Reading<Float>>): WeatherForecast {
-        val tendency = getTendency(readings)
+        val last = readings.lastOrNull() ?: return WeatherForecast(Weather.Unknown, 0f)
 
-        val confidence =
-            SolMath.clamp(
-                SolMath.norm(
-                    tendency.absoluteValue,
-                    0f,
-                    stormThreshold.absoluteValue
-                ), 0f, 1f
-            )
+        if (last.value == 0f || last.value.isNaN()) {
+            return WeatherForecast(Weather.Unknown, 0f)
+        }
 
-        return when {
-            tendency >= stormThreshold -> {
-                WeatherForecast(Weather.WorseningFast, confidence)
-            }
-            tendency >= changeThreshold -> {
-                WeatherForecast(Weather.WorseningSlow, confidence)
-            }
-            tendency <= -stormThreshold -> {
-                WeatherForecast(Weather.ImprovingFast, confidence)
-            }
-            tendency <= -changeThreshold -> {
-                WeatherForecast(Weather.ImprovingSlow, confidence)
-            }
-            else -> {
-                WeatherForecast(Weather.NoChange, 1 - confidence)
-            }
+        return if (last.value < 0.6f) {
+            return WeatherForecast(Weather.ImprovingSlow, 1 - last.value)
+        } else {
+            WeatherForecast(Weather.WorseningSlow, last.value)
         }
 
     }
-
-    private fun getTendency(readings: List<Reading<Float>>): Float {
-        val first = readings.firstOrNull() ?: return 0f
-        val last = readings.lastOrNull() ?: return 0f
-
-        val diff = last.value - first.value
-        val dt = Duration.between(first.time, last.time).seconds / 3600f
-
-        if (dt == 0f) {
-            return 0f
-        }
-
-        return diff / dt
-    }
-
 }
--- science/src/main/java/com/kylecorry/sol/science/meteorology/forecast/WeatherForecast.kt
+++ science/src/main/java/com/kylecorry/sol/science/meteorology/forecast/WeatherForecast.kt
@@ -1,3 +1,7 @@
 package com.kylecorry.sol.science.meteorology.forecast
 
-data class WeatherForecast(val weather: Weather, val confidence: Float)
+data class WeatherForecast(val weather: Weather, val confidence: Float) {
+    val isBadWeather =
+        weather == Weather.WorseningSlow || weather == Weather.WorseningFast || weather == Weather.Storm
+    val isGoodWeather = weather == Weather.ImprovingSlow || weather == Weather.ImprovingFast
+}
