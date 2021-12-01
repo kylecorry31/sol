60299c56a42e161754999313439508ee6a6087c2
Kyle Corry <kylecorry31@gmail.com>
2021-12-01 15:35:53 -0500

    Don't translate map coords to bounds

---
+++ science/src/main/java/com/kylecorry/sol/science/geology/projections/CylindricalEquidistantProjection.kt
@@ -0,0 +1,29 @@
+package com.kylecorry.sol.science.geology.projections
+
+import com.kylecorry.sol.math.SolMath.cosDegrees
+import com.kylecorry.sol.math.SolMath.toDegrees
+import com.kylecorry.sol.math.SolMath.toRadians
+import com.kylecorry.sol.math.Vector2
+import com.kylecorry.sol.units.Coordinate
+
+class CylindricalEquidistantProjection(private val scale: Float = 0f) : IMapProjection {
+
+    override fun toCoordinate(pixel: Vector2): Coordinate {
+        val longitude = (pixel.x / scale).toDouble().toDegrees()
+        val latitude = pixel.y.toDouble().toDegrees()
+        return Coordinate(latitude, longitude)
+    }
+
+    override fun toPixels(location: Coordinate): Vector2 {
+        val x = location.longitude.toRadians() * scale
+        val y = location.latitude.toRadians()
+        return Vector2(x.toFloat(), y.toFloat())
+    }
+
+    companion object {
+        fun getScaleForLatitude(latitude: Double): Float {
+            return cosDegrees(latitude).toFloat()
+        }
+    }
+
+}
---
+++ science/src/main/java/com/kylecorry/sol/science/geology/projections/MercatorProjection.kt
@@ -0,0 +1,32 @@
+package com.kylecorry.sol.science.geology.projections
+
+import com.kylecorry.sol.math.SolMath.cosDegrees
+import com.kylecorry.sol.math.SolMath.sinDegrees
+import com.kylecorry.sol.math.SolMath.toDegrees
+import com.kylecorry.sol.math.SolMath.toRadians
+import com.kylecorry.sol.math.Vector2
+import com.kylecorry.sol.units.Coordinate
+import kotlin.math.atan
+import kotlin.math.ln
+import kotlin.math.sinh
+
+class MercatorProjection(private val scale: Float = 1f) : IMapProjection {
+    override fun toCoordinate(pixel: Vector2): Coordinate {
+        val longitude = (pixel.x / scale).toDegrees()
+        val latitude = (atan(sinh(pixel.y / scale))).toDegrees()
+        return Coordinate(latitude.toDouble(), longitude.toDouble())
+    }
+
+    override fun toPixels(location: Coordinate): Vector2 {
+        val x = scale * location.longitude.toRadians()
+        val y = 0.5 * ln((1 + sinDegrees(location.latitude)) / (1 - sinDegrees(location.latitude)))
+        return Vector2(x.toFloat(), y.toFloat())
+    }
+
+    companion object {
+        fun getScaleForLatitude(latitude: Double): Float {
+            return cosDegrees(latitude).toFloat()
+        }
+    }
+
+}
