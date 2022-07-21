faa44d073de888465742df3ad9447e1916631d7a
Kyle Corry <kylecorry31@gmail.com>
2022-07-21 16:01:08 -0400

    Add reading helper methods

--- time/src/main/java/com/kylecorry/sol/time/Time.kt
+++ time/src/main/java/com/kylecorry/sol/time/Time.kt
@@ -1,5 +1,6 @@
 package com.kylecorry.sol.time
 
+import com.kylecorry.sol.units.Reading
 import java.time.*
 import java.time.temporal.Temporal
 
@@ -125,6 +126,41 @@ object Time {
 
     fun hoursBetween(first: Temporal, second: Temporal): Float {
         return Duration.between(first, second).seconds / 3600f
+    }
+
+    fun <T> getReadings(
+        date: LocalDate,
+        zone: ZoneId,
+        step: Duration,
+        valueFn: (time: ZonedDateTime) -> T
+    ): List<Reading<T>> {
+        return getReadings(
+            date.atStartOfDay().atZone(zone),
+            date.atEndOfDay().atZone(zone),
+            step,
+            valueFn
+        )
+    }
+
+    fun <T> getReadings(
+        start: ZonedDateTime,
+        end: ZonedDateTime,
+        step: Duration,
+        valueFn: (time: ZonedDateTime) -> T
+    ): List<Reading<T>> {
+        val readings = mutableListOf<Reading<T>>()
+        var time = start
+        while (time <= end) {
+            readings.add(Reading(valueFn(time), time.toInstant()))
+            time = time.plus(step)
+        }
+        return readings
+    }
+
+    fun List<Reading<*>>.duration(): Duration {
+        val start = minByOrNull { it.time } ?: return Duration.ZERO
+        val end = maxByOrNull { it.time } ?: return Duration.ZERO
+        return Duration.between(start.time, end.time)
     }
 
 }
