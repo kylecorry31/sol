fea6eb1de89118772d8c6c962e36239f5bb9a908
Kyle Corry <kylecorry31@gmail.com>
2023-05-17 21:13:22 -0400

    Improve readability of compass

--- science/src/main/java/com/kylecorry/sol/science/geology/AzimuthCalculator.kt
+++ science/src/main/java/com/kylecorry/sol/science/geology/AzimuthCalculator.kt
@@ -15,20 +15,25 @@ internal object AzimuthCalculator {
         val normGravity = Vector3Utils.normalize(gravity)
         val normMagField = Vector3Utils.normalize(magneticField)
 
-        // East vector
+        // East vector - perpendicular to gravity and magnetic field
         val east = Vector3Utils.cross(normMagField, normGravity)
         val normEast = Vector3Utils.normalize(east)
 
-        // North vector
+        // North vector - projection of magnetic field onto the ground plane
+        // This is equivalent to east X gravity
         val north = Vector3Utils.projectOnPlane(normMagField, normGravity)
         val normNorth = Vector3Utils.normalize(north)
 
         // Azimuth
         // See https://math.stackexchange.com/questions/381649/whats-the-best-3d-angular-co-ordinate-system-for-working-with-smartfone-apps
-        // This is derived from the rotation matrix
-        val x = normEast[0] + normNorth[1]
-        val y = normEast[1] - normNorth[0]
-        val azimuth = if (!(y == 0f && y == x)) atan2(y, x) else 0f
+        // North and East are perpendicular and are rotated around the down vector
+        // Therefore their orientation compared to the forward facing vector is the azimuth (acting as sine/cosine on the unit circle)
+        // Sine = east.x + north.y
+        // Cosine = east.y - north.x (north vector is flipped around the y axis when compared to east)
+
+        val sin = normEast[0] + normNorth[1]
+        val cos = normEast[1] - normNorth[0]
+        val azimuth = if (!(cos == 0f && cos == sin)) atan2(cos, sin) else 0f
 
         if (azimuth.isNaN()){
             return null
