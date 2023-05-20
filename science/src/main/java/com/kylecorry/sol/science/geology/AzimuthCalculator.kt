a059745a7cff0f1f95e4cee5a4a7750b873a3443
Kyle Corry <kylecorry31@gmail.com>
2023-05-20 09:35:45 -0400

    Revert "Switch back to ENU format to match Android"
    This reverts commit a6f61cd4b27351fb471adab447bf13ca21f2aa27.

--- science/src/main/java/com/kylecorry/sol/science/geology/AzimuthCalculator.kt
+++ science/src/main/java/com/kylecorry/sol/science/geology/AzimuthCalculator.kt
@@ -3,30 +3,26 @@ package com.kylecorry.sol.science.geology
 import com.kylecorry.sol.math.SolMath.toDegrees
 import com.kylecorry.sol.math.Vector3
 import com.kylecorry.sol.math.Vector3Utils
-import com.kylecorry.sol.math.algebra.Matrix
-import com.kylecorry.sol.math.algebra.columns
-import com.kylecorry.sol.math.algebra.createMatrix
-import com.kylecorry.sol.math.algebra.mapped
-import com.kylecorry.sol.math.algebra.rows
 import com.kylecorry.sol.units.Bearing
 import kotlin.math.atan2
 
 internal object AzimuthCalculator {
 
     fun calculate(acceleration: FloatArray, magneticField: FloatArray): Bearing? {
+        // Down vector (from gravity)
+        val down = Vector3Utils.normalize(Vector3Utils.times(acceleration, -1f), true)
+
         // East vector - perpendicular to down and magnetic field
-        val east = Vector3Utils.normalize(Vector3Utils.cross(acceleration, magneticField), true)
+        val east = Vector3Utils.normalize(Vector3Utils.cross(down, magneticField), true)
 
         // North vector - perpendicular to down and east
-        val north = Vector3Utils.normalize(Vector3Utils.cross(acceleration, east), true)
+        val north = Vector3Utils.normalize(Vector3Utils.cross(east, down), true)
 
         // Azimuth
         // https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/java/android/hardware/SensorManager.java;l=91;bpv=1;bpt=0?q=SensorManager&sq=&ss=android%2Fplatform%2Fsuperproject
         // Derived from the rotation matrix with X = Y
         // This is the projection of the east and north vectors onto the X-Y plane (just their X component)
-        val azimuth = atan2(east[0], north[0]).toDegrees() + 90
-        // Pitch: asin(-acceleration[0])
-        // Roll: atan2(-acceleration[1], acceleration[2])
+        val azimuth = atan2(-north[0], east[0]).toDegrees()
 
         if (azimuth.isNaN()) {
             return null
