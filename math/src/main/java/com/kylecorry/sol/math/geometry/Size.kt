08f939c2359dfbf675607b50aee537052c287dbe
Kyle Corry <kylecorry31@gmail.com>
2023-05-29 12:23:09 -0400

    Add Size.rotate method

--- math/src/main/java/com/kylecorry/sol/math/geometry/Size.kt
+++ math/src/main/java/com/kylecorry/sol/math/geometry/Size.kt
@@ -1,3 +1,20 @@
 package com.kylecorry.sol.math.geometry
 
-data class Size(val width: Float, val height: Float)
+import com.kylecorry.sol.math.SolMath
+import kotlin.math.abs
+
+data class Size(val width: Float, val height: Float) {
+    /**
+     * Calculates the size of the rectangle when rotated (maximum bounds)
+     * @param angle: The angle to rotate the rectangle by
+     * @return The size of the rectangle when rotated
+     */
+    fun rotate(angle: Float): Size {
+        val sinAngle = SolMath.sinDegrees(angle)
+        val cosAngle = SolMath.cosDegrees(angle)
+        return Size(
+            abs(width * cosAngle) + abs(height * sinAngle),
+            abs(width * sinAngle) + abs(height * cosAngle),
+        )
+    }
+}
