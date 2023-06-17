ad88276c3abc54f1b99d4dddbacdd05a6060f27d
Kyle Corry <kylecorry31@gmail.com>
2023-06-17 19:39:00 -0400

    Add multiple intersection for range

--- math/src/main/java/com/kylecorry/sol/math/Range.kt
+++ math/src/main/java/com/kylecorry/sol/math/Range.kt
@@ -19,4 +19,37 @@ data class Range<T : Comparable<T>>(val start: T, val end: T) {
         return Range(maxOf(start, other.start), minOf(end, other.end))
     }
 
+    companion object {
+        fun <T : Comparable<T>> intersection(
+            ranges: List<Range<T>>,
+            ignoreNoIntersection: Boolean = false,
+            stopWhenNoIntersection: Boolean = false
+        ): Range<T>? {
+            if (ranges.isEmpty()) {
+                return null
+            }
+
+            var intersection: Range<T>? = ranges[0]
+            for (i in 1 until ranges.size) {
+                val range = ranges[i]
+                val newIntersection = intersection?.intersection(range)
+                intersection = if (newIntersection == null) {
+                    if (stopWhenNoIntersection) {
+                        return intersection
+                    }
+                    if (ignoreNoIntersection) {
+                        intersection
+                    } else {
+                        return null
+                    }
+                } else {
+                    newIntersection
+                }
+            }
+
+            return intersection
+
+        }
+    }
+
 }
