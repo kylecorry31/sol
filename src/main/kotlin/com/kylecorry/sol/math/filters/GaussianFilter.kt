279336945ddd8c312061e2f5e797c13317a91a33
Kyle Corry <kylecorry31@gmail.com>
2023-12-13 19:04:32 -0500

    Add gaussian filter

---
+++ src/main/kotlin/com/kylecorry/sol/math/filters/GaussianFilter.kt
@@ -0,0 +1,45 @@
+package com.kylecorry.sol.math.filters
+
+import com.kylecorry.sol.math.RingBuffer
+import com.kylecorry.sol.math.SolMath.positive
+import com.kylecorry.sol.math.SolMath.real
+import com.kylecorry.sol.math.statistics.GaussianDistribution
+import com.kylecorry.sol.math.statistics.Statistics
+
+class GaussianFilter(samples: Int, private val defaultError: Float) : IFilter {
+
+    private val buffer = RingBuffer<GaussianDistribution>(samples)
+
+    var value: Float = 0f
+        private set
+
+    var error: Float? = null
+        private set
+
+    val hasValidReading: Boolean
+        get() = buffer.isFull()
+
+    fun filter(measurement: Float, error: Float): Float {
+        val variance = error
+            .real(defaultError)
+            .positive(defaultError)
+
+        val distribution = GaussianDistribution(
+            measurement.real(0f),
+            variance
+        )
+
+        buffer.add(distribution)
+        val calculated = Statistics.joint(buffer.toList())
+        if (calculated != null) {
+            this.value = calculated.mean.real(0f)
+            this.error =
+                calculated.standardDeviation.real(defaultError).positive(defaultError)
+        }
+        return this.value
+    }
+
+    override fun filter(measurement: Float): Float {
+        return filter(measurement, defaultError)
+    }
+}
