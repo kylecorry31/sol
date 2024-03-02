5b4bd336317c62fdb58e189525e267bf4d4d2935
Kyle Corry <kylecorry31@gmail.com>
2024-03-02 18:32:37 -0500

    Remove junit 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,6 @@ repositories {
 dependencies {
     implementation("com.github.kylecorry31:Geo-Coordinate-Conversion-Java:master")
     implementation("com.github.kylecorry31:osgb:v1.0.4")
-    testImplementation("org.junit.platform:junit-platform-runner:1.10.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.28.0")
