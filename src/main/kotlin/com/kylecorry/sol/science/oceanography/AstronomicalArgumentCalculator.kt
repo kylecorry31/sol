2bbeeef079f677a1b5d2db77b55275b489fa3ea0
Kyle Corry <kylecorry31@gmail.com>
2024-08-02 06:02:47 -0400

    Extract tide constants

--- src/main/kotlin/com/kylecorry/sol/science/oceanography/AstronomicalArgumentCalculator.kt
+++ src/main/kotlin/com/kylecorry/sol/science/oceanography/AstronomicalArgumentCalculator.kt
@@ -10,37 +10,49 @@ internal object AstronomicalArgumentCalculator {
     }
 
     private fun get2022(constituent: TideConstituent): Float {
+        val Q1 = 45.003f
+        val O1 = 44.031f
+        val P1 = 348.805f
+        val K1 = 3.577f
+        val N2 = 46.36f
+        val M2 = 45.013f
+        val S2 = 0.113f
+        val K2 = 186.65f
+
+        // Other
+        val L2 = 213.7788f
+
         return when (constituent) {
-            TideConstituent.M2 -> 45.013f
-            TideConstituent.S2 -> 0.113f
-            TideConstituent.N2 -> 46.36f
-            TideConstituent.K1 -> 3.577f
-            TideConstituent.M4 -> 45.013f * 2
-            TideConstituent.O1 -> 44.031f
-            TideConstituent.M6 -> 3 * 45.013f
-            TideConstituent.MK3 -> 45.013f + 3.577f // M2 + K1
-            TideConstituent.S4 -> 0.113f * 2 // S2 + S2
-            TideConstituent.MN4 -> 45.013f + 46.36f // M2 + N2
-            TideConstituent.NU2 -> 45.013f // M2
-            TideConstituent.S6 -> 0.113f * 3 // S2 + S2 + S2
-            TideConstituent.MU2 -> 45.013f * 2 - 0.113f // M2 * 2 - S2
-            TideConstituent._2N2 -> 45.013f // M2
-            TideConstituent.LAM2 -> 45.013f // M2
+            TideConstituent.M2 -> M2
+            TideConstituent.S2 -> S2
+            TideConstituent.N2 -> N2
+            TideConstituent.K1 -> K1
+            TideConstituent.M4 -> M2 * 2
+            TideConstituent.O1 -> O1
+            TideConstituent.M6 -> M2 * 3
+            TideConstituent.MK3 -> M2 + K1
+            TideConstituent.S4 -> S2 * 2
+            TideConstituent.MN4 -> M2 + N2
+            TideConstituent.NU2 -> M2
+            TideConstituent.S6 -> S2 * 3
+            TideConstituent.MU2 -> M2 * 2 - S2
+            TideConstituent._2N2 -> M2
+            TideConstituent.LAM2 -> M2
             TideConstituent.S1 -> 0f
             TideConstituent.SSA -> 0f
             TideConstituent.SA -> 0f
-            TideConstituent.MSF -> 0.113f - 45.013f // S2 - M2
-            TideConstituent.RHO -> 45.013f - 3.577f // NU2 - K1
-            TideConstituent.Q1 -> 45.003f
+            TideConstituent.MSF -> S2 - M2
+            TideConstituent.RHO -> M2 - K1 // NU2 - K1
+            TideConstituent.Q1 -> Q1
             TideConstituent.T2 -> 0f
             TideConstituent.R2 -> 0f
-            TideConstituent.P1 -> 348.805f
-            TideConstituent._2SM2 -> 2 * 0.113f - 45.013f // 2 * S2 - M2
-            TideConstituent.L2 -> 213.7788f // Calculated
-            TideConstituent._2MK3 -> 45.013f + 44.031f // M2 + O1
-            TideConstituent.K2 -> 186.65f
-            TideConstituent.M8 -> 45.013f * 4 // M2 * 4
-            TideConstituent.MS4 -> 45.013f + 0.113f // M2 + S2
+            TideConstituent.P1 -> P1
+            TideConstituent._2SM2 -> 2 * S2 - M2
+            TideConstituent.L2 -> L2
+            TideConstituent._2MK3 -> M2 + O1
+            TideConstituent.K2 -> K2
+            TideConstituent.M8 -> M2 * 4
+            TideConstituent.MS4 -> M2 + S2
             TideConstituent.Z0 -> 0f
         }
     }
--- src/main/kotlin/com/kylecorry/sol/science/oceanography/NodeFactorCalculator.kt
+++ src/main/kotlin/com/kylecorry/sol/science/oceanography/NodeFactorCalculator.kt
@@ -13,37 +13,49 @@ internal object NodeFactorCalculator {
     }
 
     private fun get2022(constituent: TideConstituent): Float {
+        val Q1 = 1.102f
+        val O1 = 1.140f
+        val P1 = 0.995f
+        val K1 = 1.081f
+        val N2 = 0.978f
+        val M2 = 0.975f
+        val S2 = 1.001f
+        val K2 = 1.214f
+
+        // Other
+        val L2 = 1.2437f
+
         return when (constituent) {
-            TideConstituent.M2 -> 0.975f
-            TideConstituent.S2 -> 1.001f
-            TideConstituent.N2 -> 0.978f
-            TideConstituent.K1 -> 1.081f
-            TideConstituent.M4 -> square(0.975f)
-            TideConstituent.O1 -> 1.140f
-            TideConstituent.M6 -> cube(0.975f)
-            TideConstituent.MK3 -> 0.975f * 1.081f // M2 * K1
-            TideConstituent.S4 -> square(1.001f) // S2 * S2
-            TideConstituent.MN4 -> 0.975f * 0.978f // M2 * N2
-            TideConstituent.NU2 -> 0.975f // M2
-            TideConstituent.S6 -> cube(1.001f) // S2 * S2 * S2
-            TideConstituent.MU2 -> square(0.975f) * 1.001f // M2 * M2 * S2
-            TideConstituent._2N2 -> 0.975f // M2
-            TideConstituent.LAM2 -> 0.975f // M2
+            TideConstituent.M2 -> M2
+            TideConstituent.S2 -> S2
+            TideConstituent.N2 -> N2
+            TideConstituent.K1 -> K1
+            TideConstituent.M4 -> square(M2)
+            TideConstituent.O1 -> O1
+            TideConstituent.M6 -> cube(M2)
+            TideConstituent.MK3 -> M2 * K1
+            TideConstituent.S4 -> square(S2)
+            TideConstituent.MN4 -> M2 * N2
+            TideConstituent.NU2 -> M2
+            TideConstituent.S6 -> cube(S2)
+            TideConstituent.MU2 -> square(M2) * S2
+            TideConstituent._2N2 -> M2
+            TideConstituent.LAM2 -> M2
             TideConstituent.S1 -> 1f
             TideConstituent.SSA -> 1f
             TideConstituent.SA -> 1f
-            TideConstituent.MSF -> 1.001f * 0.975f // S2 * M2
-            TideConstituent.RHO -> 0.975f * 1.081f // NU2 * K1
-            TideConstituent.Q1 -> 1.102f
+            TideConstituent.MSF -> S2 * M2
+            TideConstituent.RHO -> M2 * K1 // NU2 * K1
+            TideConstituent.Q1 -> Q1
             TideConstituent.T2 -> 1f
             TideConstituent.R2 -> 1f
-            TideConstituent.P1 -> 0.995f
-            TideConstituent._2SM2 -> square(1.001f) * 0.975f // 2 * S2 * M2
-            TideConstituent.L2 -> 1.2437f
-            TideConstituent._2MK3 -> 0.975f * 1.140f // M2 * O1
-            TideConstituent.K2 -> 1.214f
-            TideConstituent.M8 -> power(0.975f, 4) // M2 * M2 * M2 * M2
-            TideConstituent.MS4 -> 0.975f * 1.001f
+            TideConstituent.P1 -> P1
+            TideConstituent._2SM2 -> square(S2) * M2
+            TideConstituent.L2 -> L2
+            TideConstituent._2MK3 -> M2 * O1
+            TideConstituent.K2 -> K2
+            TideConstituent.M8 -> power(M2, 4)
+            TideConstituent.MS4 -> M2 * S2
             TideConstituent.Z0 -> 1f
         }
     }
