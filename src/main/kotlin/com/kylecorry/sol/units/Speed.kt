81285baaf28f2a1f745a53f05c83410e35e695ba
Kyle Corry <kylecorry31@gmail.com>
2025-03-05 15:49:15 -0500

    Revert "Replace Time with Quantity"
    This reverts commit f89d0ec10552d15c90fd5adff4e5d7a7020612a4.

--- src/main/kotlin/com/kylecorry/sol/units/Speed.kt
+++ src/main/kotlin/com/kylecorry/sol/units/Speed.kt
@@ -1,10 +1,10 @@
 package com.kylecorry.sol.units
 
-data class Speed(val speed: Float, val distanceUnits: Distance, val timeUnits: Time) {
+data class Speed(val speed: Float, val distanceUnits: Distance, val timeUnits: TimeUnits) {
 
-    fun convertTo(newDistance: Distance, newTimeUnits: Time): Speed {
+    fun convertTo(newDistance: Distance, newTimeUnits: TimeUnits): Speed {
         val distance = Quantity(speed, distanceUnits).convertTo(newDistance).amount
-        val newSpeed = (distance / timeUnits.multiplierToBase) * newTimeUnits.multiplierToBase
+        val newSpeed = (distance / timeUnits.seconds) * newTimeUnits.seconds
         return Speed(newSpeed, newDistance, newTimeUnits)
     }
 
