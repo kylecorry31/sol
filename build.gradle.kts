3b1df4584fa82a37ca08a177813fe9a16b09f4c8
Kyle Corry <kylecorry31@gmail.com>
2025-06-02 20:27:57 -0400

    Update junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,10 +50,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.0")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.28.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.0")
 }
 
 tasks.test {
