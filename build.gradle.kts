f51e5b3717e5a0b0797ee234e90bcf9a55066e38
Kyle Corry <kylecorry31@gmail.com>
2025-09-12 07:42:10 -0400

    Update version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
-val versionName = "12.2.0"
+val versionName = "13.0.0"
 
 mavenPublishing {
     coordinates("com.kylecorry", "sol", versionName)
