e8c494545cd221794ec57012ec623d9663ab8279
Kyle Corry <kylecorry31@gmail.com>
2025-10-26 12:49:23 -0400

    Add more water level calculators

---
+++ src/main/kotlin/com/kylecorry/sol/science/oceanography/waterlevel/GapWaterLevelCalculator.kt
@@ -0,0 +1,31 @@
+package com.kylecorry.sol.science.oceanography.waterlevel
+
+import com.kylecorry.sol.math.Vector2
+import com.kylecorry.sol.math.analysis.Trigonometry
+import com.kylecorry.sol.science.oceanography.Tide
+import com.kylecorry.sol.time.Time
+import java.time.ZonedDateTime
+
+class GapWaterLevelCalculator(
+    private val first: Tide,
+    private val second: Tide,
+    private val approximateFrequency: Float
+) :
+    IWaterLevelCalculator {
+
+    private val wave by lazy {
+        val firstVec = Vector2(getX(first.time), first.height ?: (if (first.isHigh) 1f else -1f))
+        val secondVec =
+            Vector2(getX(second.time), second.height ?: (if (second.isHigh) 1f else -1f))
+        Trigonometry.connect(firstVec, secondVec, approximateFrequency)
+    }
+
+    override fun calculate(time: ZonedDateTime): Float {
+        return wave.calculate(getX(time))
+    }
+
+    private fun getX(time: ZonedDateTime): Float {
+        return Time.hoursBetween(first.time, time)
+    }
+
+}
---
+++ src/main/kotlin/com/kylecorry/sol/science/oceanography/waterlevel/PiecewiseWaterLevelCalculator.kt
@@ -0,0 +1,11 @@
+package com.kylecorry.sol.science.oceanography.waterlevel
+
+import com.kylecorry.sol.math.Range
+import java.time.ZonedDateTime
+
+class PiecewiseWaterLevelCalculator(private val calculators: List<Pair<Range<ZonedDateTime>, IWaterLevelCalculator>>) :
+    IWaterLevelCalculator {
+    override fun calculate(time: ZonedDateTime): Float {
+        return calculators.firstOrNull { it.first.contains(time) }?.second?.calculate(time) ?: 0f
+    }
+}
