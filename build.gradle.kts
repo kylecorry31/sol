f07de9a0928beb033d703d5b17ff5e8a79d63180
Kyle Corry <kylecorry31@gmail.com>
2026-02-20 18:23:29 -0500

    build: update version number
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
-val versionName = "16.0.0"
+val versionName = "16.0.1"
 
 mavenPublishing {
     coordinates("com.kylecorry", "sol", versionName)
