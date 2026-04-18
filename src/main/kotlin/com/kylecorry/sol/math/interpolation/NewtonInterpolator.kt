f924212baecb39f5105e1bf6aa5175d41d0d1693
Kyle Corry <kylecorry31@gmail.com>
2026-04-18 13:42:32 -0400

    refactor: address VarCouldBeVal rule
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- src/main/kotlin/com/kylecorry/sol/math/interpolation/NewtonInterpolator.kt
+++ src/main/kotlin/com/kylecorry/sol/math/interpolation/NewtonInterpolator.kt
@@ -5,7 +5,7 @@ import com.kylecorry.sol.math.regression.NewtonPolynomialRegression
 
 class NewtonInterpolator(points: List<Vector2>, order: Int = points.size - 1) : Interpolator {
     private val regression = NewtonPolynomialRegression(order)
-    private var polynomial = regression.fit(points)
+    private val polynomial = regression.fit(points)
 
     override fun interpolate(x: Float): Float {
         return polynomial.evaluate(x)
